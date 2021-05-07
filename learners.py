48b7492c5421f3c4eafd940750d65c8207410302
joonholee99 <joonholee99@gmail.com>
2021-05-07 22:59:04 +0900

    Learners(ing)

--- learners.py
+++ learners.py
@@ -14,5 +14,33 @@ from agent import Agent
 from networks import Network, DNN
 from visualizer import Visualizer 
 
-# class ReinforcementLearner:
+class ReinforcementLearner:
+    __metaclass__ = abc.ABCMeta
+    lock = threading.Lock()
+
+    def __init__(self, rl_method = 'rl', stock_code = None, chart_data = None,
+                traiding_data = None, min_trading_unit = 1, max_trading_unit = 2,
+                delayed_reward_threshold = 0.05, net = 'dnn', num_steps = 1, lr = 0.001,
+                value_network = None, policy_network = None, output_path = '', reuse_models = True):
+                
+                assert min_trading_unit > 0
+                assert max_trading_unit > 0
+                assert max_trading_unit > min_trading_unit
+                assert num_steps > 0
+                assert lr > 0
+
+                #강화학습 기법 설정
+                self.rl_method = rl_method
+
+                #환경 설정
+                self.stock_code = stock_code
+                self.chart_data = chart_data
+                self.environment = Environment(chart_data)
+
+                # 에이전트 설정
+                self.agent = Agent(self.environment, min_trading_unit= min_trading_unit, max_trading_unit= max_trading_unit, 
+                                    delayed_reward_threshold = delayed_reward_threshold)
+
+                
+
     
