5a7253efd031cf0549262f237d1ab087a0b826f5
joonholee99 <joonholee99@gmail.com>
2021-05-26 21:23:38 +0900

    5/26 Quanty Lab에서 GIT 가져옴

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 class Environment:
-    PRICE_INDEX = 4 # place of Last price
+    PRICE_IDX = 4  # 종가의 위치
 
-    def __init__(self, char_data=None):
+    def __init__(self, chart_data=None):
         self.chart_data = chart_data
         self.observation = None
         self.idx = -1
@@ -11,20 +11,16 @@ class Environment:
         self.idx = -1
 
     def observe(self):
-        if len(self.chart_data) > self.idx:
+        if len(self.chart_data) > self.idx + 1:
             self.idx += 1
             self.observation = self.chart_data.iloc[self.idx]
-
             return self.observation
-        
         return None
-    
+
     def get_price(self):
         if self.observation is not None:
-            return self.observation[self.PRICE_INDEX]
-        
+            return self.observation[self.PRICE_IDX]
         return None
 
     def set_chart_data(self, chart_data):
         self.chart_data = chart_data
-
